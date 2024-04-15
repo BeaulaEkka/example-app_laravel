0d8a756a505841f5288acf525765e430bc67df9d
Your Name <ekka.beaula@gmail.com>
2024-04-15 21:34:20 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Models\job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('home');
+});
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs' => job::all()]);;
+});
+
+Route::get('/jobs/{id}', function ($id) {
+
+    $job = job::find($id);
+    // dd($job);
+    return view('job', ['job' => $job]);
+
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
